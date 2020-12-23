44ca9a9784d0a7f860b658d3b3d2408afe53c86e
Khalid Benlyazid <kbenlyaz@e1r6p6.1337.ma>
2020-12-23 19:22:35 +0100

    workig on 301 redirection

--- srcs/nginx/run.sh
+++ srcs/nginx/run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 mkdir /run/openrc
 touch /run/openrc/softlevel
+rm /etc/nginx/conf.d/default.conf
+mv /default.conf /etc/nginx/conf.d/
 rc-status
-echo "daemon off;" >> /etc/nginx/nginx.conf
+#echo "daemon off;" >> /etc/nginx/nginx.conf
 rc-service nginx start
-#/bin/sh
+/bin/sh
