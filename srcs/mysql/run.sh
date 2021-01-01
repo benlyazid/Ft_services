cc498339841a69f445a4c805e4c98535fa0115d0
Khalid Benlyazid <kbenlyaz@e1r6p6.1337.ma>
2021-01-01 11:32:15 +0100

    acces to mysql outside

--- srcs/mysql/run.sh
+++ srcs/mysql/run.sh
@@ -1,6 +1,11 @@
 apk add bash
 rc-status
+rm /etc/my.cnf.d/mariadb-server.cnf
 touch /run/openrc/softlevel
 /etc/init.d/mariadb setup
+mv /mariadb-server.cnf /etc/my.cnf.d/mariadb-server/
 rc-service mariadb start
-/bin/bash
+while true
+do 
+sleep 1;
+done
