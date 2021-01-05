c206f3584f820ed3330f7e7111a71b6ee59ab145
Khalid Benlyazid <kbenlyaz@e1r6p6.1337.ma>
2021-01-05 12:38:56 +0100

    working on phpmyadmin

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 minikube start
 docker-machine start
 docker-machine env
+eval $(docker-machine env) 
 eval $(minikube docker-env)
 docker build -t nginx srcs/nginx/.
 docker build -t  mysql srcs/mysql/.
 #metalLb
-kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.9.3/manifests/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.9.3/manifests/metallb.yaml
-kubectl apply srcs/metallb.yml
-#RUN Continaires
-kubectl apply -f srcs/nginx/deployment.yaml
-kubectl apply -f srcs/nginx/service.yaml
-kubectl apply -f srcs/mysql/deployment.yaml
-kubectl apply -f srcs/mysql/service_mysql.yaml
+#kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.9.3/manifests/namespace.yaml
+#kubectl apply -f https://raw.githubusercontent.com/google/metallb/v0.9.3/manifests/metallb.yaml
+#kubectl apply srcs/metallb.yml
+##RUN Continaires
+#kubectl apply -f srcs/nginx/deployment.yaml
+#kubectl apply -f srcs/nginx/service.yaml
+#kubectl apply -f srcs/mysql/deployment.yaml
+#kubectl apply -f srcs/mysql/service_mysql.yaml
--- srcs/phpMyAdmin/php_run.sh
+++ srcs/phpMyAdmin/php_run.sh
@@ -1,14 +1,16 @@
 apk update
 apk add nginx
 apk add openrc
+rm /etc/nginx/conf.d/default.conf
+mv /default.conf /etc/nginx/conf.d/
 mkdir -p /usr/share/webapps/
-cd /usr/share/webapps
+cd /usr/share
 wget http://files.directadmin.com/services/all/phpMyAdmin/phpMyAdmin-5.0.2-all-languages.tar.gz
 tar zxvf phpMyAdmin-5.0.2-all-languages.tar.gz
 apk add php7 php7-fpm php7-mysqli php7-opcache php7-gd php7-mysqli php7-zlib php7-curl php7-phar php7-json php7-mbstring 
 mv phpMyAdmin-5.0.2-all-languages phpmyadmin
 chmod -R 777 /usr/share/webapps/
-ln -s /usr/share/webapps/phpmyadmin/ /var/www/localhost/htdocs/phpmyadmin
+ln -s /usr/share/phpmyadmin/ /var/www/localhost/htdocs/phpmyadmin
 mkdir /run/openrc/
 touch /run/openrc/softlevel
 rc-status
