9e75680b590a3246a24810b4f9d71405c763dd46
Khalid Benlyazid <kbenlyaz@e1r6p6.1337.ma>
2021-01-23 12:38:47 +0100

    working on telegraf

--- srcs/nginx/run.sh
+++ srcs/nginx/run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 mkdir /run/openrc
 apk add openssh
+apk add openssh-keygen
 adduser admin < user_file
 ssh-keygen -A
 touch /run/openrc/softlevel
@@ -8,10 +9,12 @@ rm /etc/nginx/conf.d/default.conf
 mv /default.conf /etc/nginx/conf.d/
 wget https://dl.influxdata.com/telegraf/releases/telegraf-1.17.0_linux_amd64.tar.gz
 tar -zxvf telegraf-1.17.0_linux_amd64.tar.gz 
+cp telegraf.conf telegraf-1.17.0/usr/bin/
 rc-status
 rc-service nginx start
 service sshd start
-
+apk add libc6-compat
+./telegraf-1.17.0/usr/bin/telegraf --config /telegraf-1.17.0/usr/bin/telegraf.conf 
 while true
 do 
 sleep 1;
